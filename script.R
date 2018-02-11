67b37e4fc84fea396f784c35468ebad5cc281a05
Dane Hager <dane.hager@gmail.com>
2018-02-11 16:07:21 +0000

    commit 2

--- script.R
+++ script.R
@@ -34,4 +34,29 @@ BrewCrew1982 <- Teams %>%
   filter(franchID=="MIL" & yearID==1982)
 
 #initial view shows 1982 team had 163 games vs 162 games for the 2016 team.
-#Actually 
+#Actually I could probably use the %in%, rather than have two data tables
+
+BrewCrewCompare<- Teams %>%
+  filter(franchID=="MIL" & yearID %in% c(1982, 2016))
+
+#To now compute some per game metrics to compare data across years
+BrewCrewCompare %>%
+  mutate(HPG=H/G, 
+         RPG=R/G,
+         X2BPG=X2B/G,
+         X3BPG=X3B/G,
+         HRPG=HR/G,
+         ERPG=ER/G,
+         HAPG=HA/G,
+         HRAPG=HRA/G,
+         EPG=E/G,
+         HRAperHA=HRA/HA)
+
+#Just a quick glance through the tables shows:
+# 1.) HPG up nearly 2 hits in 1982
+# 2.) RPG up 1.3 runs in 1982
+# 3.) Most offensive metrics up in 1982 (H, R, 2B, 3B, HR)
+# 4.) Earned Runs are nearly flat (649 vs 650 in total)
+# 5.) However, that ER in 1982 resulted from a heavier percentage of hits that were not HR
+#   a.) HAPG is up by 64 in 1982
+#   b.) HRAPG is down by 26 HRA in total
